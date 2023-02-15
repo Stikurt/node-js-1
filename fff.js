4a9e8531e867444c2ec87d763eadb06d73cb228b
Stikurt <112864142+Stikurt@users.noreply.github.com>
2023-02-15 14:04:31 +0600

    Add files via upload

---
+++ fff.js
@@ -0,0 +1,27 @@
+//вывод 150 hello world
+ for (let i = 0; i < 150; i++) {
+     console.log("hello world");
+ }
+//вывод в консоль чётных чисел от 2 до 20
+/*for (let i = 2; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log( i );
+    }
+ }
+*/
+//Получить аргумент в функции. Если он больше 0 - вывести в консоль "Hello", если меньше - "olleH":
+/*let a = 5;
+let b = 0;
+if (a > b) {
+    console.log('Hello');
+}
+else{
+    console.log('olleH');
+    } 
+if (a < b) {
+    console.log('Hello');
+    }
+else{
+    console.log('olleH');
+    }
+    */
